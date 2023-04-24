3a5a4b8471b5cb3ca065e990709e45ccbe9614ef
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2023-04-24 17:26:53 +0200

    update test structure

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,3 +1,5 @@
+from os import path as op
+
 import xarray as xr
 
 from analysis_functions.download import download_file
@@ -5,6 +7,13 @@ from analysis_functions.download import download_file
 url = "https://sandbox.zenodo.org/record/1185969/files/e065000e_c167_198001.nc"
 
 
-filename = download_file(url)
-ds = xr.open_dataset(filename)
-print(ds)
+def test_download():
+    filename = download_file(url)
+    assert filename == op.basename(url)
+    ds = xr.open_dataset(filename)
+    print(ds)
+    return ds
+
+
+if __name__ == "__main__":
+    test_download()
