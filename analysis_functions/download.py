7e606a982252954cf03531e8aba8e4e8eaa7f2a8
Christina Asmus <christina.asmus@hzg.de>
2023-04-27 17:28:12 +0200

    added download script

--- analysis_functions/download.py
+++ analysis_functions/download.py
@@ -20,3 +20,21 @@ def download_file(url):
                 # if chunk:
                 f.write(chunk)
     return local_filename
+
+
+def download_file_in_dir(url, data_dir):
+    local_filename = op.basename(url)
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        total_size_in_bytes = int(r.headers.get("content-length", 0))
+        chunk_size = 8192
+        progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+        with open(str(data_dir) + "/" + str(local_filename), "wb") as f:
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                progress_bar.update(len(chunk))
+                # if chunk:
+                f.write(chunk)
+    return local_filename
---
+++ download_zenodo.py
@@ -0,0 +1,33 @@
+import os
+import tarfile
+from os import path as op
+
+import xarray as xr
+
+from analysis_functions.download import download_file_in_dir
+
+files = ["067017.tar.gz", "067019.tar.gz", "067020.tar.gz", "067015", "067016"]
+url = "https://zenodo.org/record/7867329/files/"
+
+# create folder for data
+dir_working = os.getcwd()
+# creates dir in parent directory
+# dir_out = os.path.join(os.pardir, "data")
+dir_out = "data"
+if not os.path.exists(dir_out):
+    os.makedirs(dir_out)
+print("Data directory is: ", dir_out)
+
+
+def download(singlefile):
+    filename = download_file_in_dir(url + str(singlefile), dir_out)
+    assert filename == op.basename(url + str(singlefile))
+    print("Downloading", str(filename))
+    return filename
+
+
+for singlefile in files:
+    download(singlefile)
+    # unpack tarball
+    file = tarfile.open(os.path.join(dir_out, singlefile))
+    file.extractall(str(dir_out))
